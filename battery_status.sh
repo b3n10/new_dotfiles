1792e34102a50d24d491822726d4b567104a4702
b3n10 <francisben.lleve@gmail.com>
2017-04-05 09:52:02 +0800

    Updated battery status

--- battery_status.sh
+++ battery_status.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 BATTINFO=`acpi -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:05:00 ]]; then
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]]; then
 	env DISPLAY=:0.0 /usr/bin/i3-nagbar -m "$BATTINFO"
 fi
 
